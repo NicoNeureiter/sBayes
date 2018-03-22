688ada5a1ab22adea6a7585d4c048adaae9bd0ea
nicon <nico.neureiter@gmail.com>
2018-03-22 19:34:03 +0100

    Minor restructure of config.py

--- src/config.py
+++ src/config.py
@@ -15,16 +15,16 @@ P_CONTACT = 0.8
 
 
 # Sampling parameters
-N_STEPS = 5000
+N_STEPS = 3000
 """int: Number of MCMC steps."""
 
-N_SAMPLES = 3
+N_SAMPLES = 10
 """int: Number of generated samples."""
 
 MIN_SIZE = 5
 """int: The minimum size for the contact zones."""
 
-MAX_SIZE = 10
+MAX_SIZE = 100
 """int: The maximum size for the contact zones."""
 
 P_SWAP = 0.5  # TODO remove (replaced by P_TRANSITION_MODE)
@@ -37,30 +37,20 @@ temperature."""
 
 P_TRANSITION_MODE = {
     'swap': 0.5,
-    'grow': 0.25,
-    'shrink': 0.25}
-assert sum(P_TRANSITION_MODE.values()) == 1.
+    'grow': 0.75,
+    'shrink': 1.}
+assert P_TRANSITION_MODE['swap'] <= P_TRANSITION_MODE['grow'] \
+                                 <= P_TRANSITION_MODE['shrink'] == 1.
 """list: Frequency of performing a 'swap', 'grow' or 'shrink' step, respectively (has 
 to sum up to 1)."""
 
 
 # Config flags
-FEATURE_LIKELIHOOD_MODES = ['generative', 'binom_test', 'binom_test_2']
-"""list: All implemented modes for the likelihood function."""
 
-FEATURE_LL_MODE = FEATURE_LIKELIHOOD_MODES[1]
-"""str: The switch for which likelihood to use."""
-
-GEO_LIKELIHOOD_MODES = ['none', 'gaussian', 'empirical']
-"""list: All implemented modes for the geo-likelihood"""
-
-GEO_LL_MODE = GEO_LIKELIHOOD_MODES[2]
-"""str: The switch for the geo-likelihood to use."""
-
-RESTART_CHAIN = False
+RESTART_CHAIN = True
 """bool: Restart the chain from a random point after every sample."""
 
-SIMULATED_ANNEALING = True
+SIMULATED_ANNEALING = False
 """bool: Slowly increase a temperature parameter to smoothly blend from sampling from a
 uniform distribution to the actual likelihood. Should help with separated modes."""
 
@@ -68,13 +58,31 @@ RELOAD_DATA = True
 """bool: Reload the data from the DB, pre-process it and dump it."""
 
 NUMBER_PARALLEL_ZONES = 3
-"""int: Number of parallel contact zones"""
-# Parameters for geo-likelihood
+"""int: Number of parallel contact zones."""
+
+PLOT_SAMPLES = True
+"""bool: Plot the every sample during MCMC run."""
+
+
+# Parameters for likelihood
+
+FEATURE_LIKELIHOOD_MODES = ['generative', 'binom_test', 'binom_test_2']
+FEATURE_LL_MODE = FEATURE_LIKELIHOOD_MODES[1]
+"""str: Switch for which feature-likelihood to use."""
+
+GEO_LIKELIHOOD_MODES = ['none', 'gaussian', 'empirical']
+GEO_LL_MODE = GEO_LIKELIHOOD_MODES[2]
+"""str: Switch for the geo-likelihood to use."""
+
+GEO_ECDF_TYPES = ['mst', 'delaunay', 'complete']
+GEO_ECDF_TYPE = GEO_ECDF_TYPES[1]
+"""str: Switch for the type of sub-graph to use for the empirical geo-likelihood."""
+
 GEO_LIKELIHOOD_WEIGHT = 1
-"""float: The weight of the geo-likelihood as compared to the feature likelihood"""
+"""float: The weight of the geo-likelihood as compared to the feature likelihood."""
 
 SAMPLES_PER_ZONE_SIZE = 1000
-"""int: The number of samples for generating the empirical geo-likelihood"""
+"""int: The number of samples for generating the empirical geo-likelihood."""
 
 
 # Names of DB tables
