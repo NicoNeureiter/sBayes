db9a60ebc98c0193f69b1599335644271d11b23a
nicon <nico.neureiter@gmail.com>
2018-04-27 14:21:30 +0200

    renamed modes, added path for random_walk_covariance

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
+
 # Simulate contact zones and background distribution
 TOTAL_N_FEATURES = 30
 """The total number of simulated features"""
@@ -27,14 +28,10 @@ N_SAMPLES = 10
 MIN_SIZE = 5
 """int: The minimum size for the contact zones."""
 
-MAX_SIZE = 100
+MAX_SIZE = 80
 """int: The maximum size for the contact zones."""
 
-P_SWAP = 0.5  # TODO remove (replaced by P_TRANSITION_MODE)
-"""float: Frequency of 'swap' steps, where a node gets replaced by another (size 
-remains constant)."""
-
-ALPHA_ANNEALING = 1.5
+ALPHA_ANNEALING = 1.
 """float: The parameter controlling the cooling schedule for the simulated annealing 
 temperature."""
 
@@ -60,31 +57,31 @@ uniform distribution to the actual likelihood. Should help with separated modes.
 RELOAD_DATA = False
 """bool: Reload the data from the DB, pre-process it and dump it."""
 
-NUMBER_PARALLEL_ZONES = 3
+NUMBER_PARALLEL_ZONES = 4
 """int: Number of parallel contact zones."""
 
-PLOT_SAMPLES = True
+PLOT_SAMPLES = False
 """bool: Plot the every sample during MCMC run."""
 
 
 # Parameters for likelihood
 
-FEATURE_LIKELIHOOD_MODES = ['generative', 'binom_test', 'binom_test_2']
-FEATURE_LL_MODE = FEATURE_LIKELIHOOD_MODES[1]
+FEATURE_LIKELIHOOD_MODES = ['generative', 'particularity']
+FEATURE_LL_MODE = FEATURE_LIKELIHOOD_MODES[0]
 """str: Switch for which feature-likelihood to use."""
 
-GEO_LIKELIHOOD_MODES = ['none', 'gaussian', 'empirical']
-GEO_LL_MODE = GEO_LIKELIHOOD_MODES[2]
+GEO_LIKELIHOOD_MODES = ['generative', 'particularity', 'none']
+GEO_LL_MODE = GEO_LIKELIHOOD_MODES[0]
 """str: Switch for the geo-likelihood to use."""
 
 GEO_ECDF_TYPES = ['mst', 'delaunay', 'complete']
-GEO_ECDF_TYPE = GEO_ECDF_TYPES[1]
+GEO_ECDF_TYPE = GEO_ECDF_TYPES[0]
 """str: Switch for the type of sub-graph to use for the empirical geo-likelihood."""
 
-GEO_LIKELIHOOD_WEIGHT = 1
+GEO_LIKELIHOOD_WEIGHT = 1.
 """float: The weight of the geo-likelihood as compared to the feature likelihood."""
 
-SAMPLES_PER_ZONE_SIZE = 10000
+SAMPLES_PER_ZONE_SIZE = 1000
 """int: The number of samples for generating the empirical geo-likelihood."""
 
 
@@ -100,6 +97,7 @@ FEATURE_PROB_PATH = 'data/processed/feature_prob.pkl'
 LOOKUP_TABLE_PATH = 'data/processed/lookup_table.pkl'
 CONTACT_ZONES_PATH = 'data/processed/contact_zones.pkl'
 ECDF_GEO_PATH = 'data/processed/ecdf_geo.pkl'
+RANDOM_WALK_COV_PATH = 'data/processed/random_walk_cov.pkl'
 
 # Path for results
 MCMC_RESULTS_PATH = 'data/results/mcmc_results.pkl'
