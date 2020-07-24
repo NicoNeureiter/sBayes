4ae3b98cbd01ecb705ccb00f4828ecb9086f72b6
derpetermann <peter.ranacher@gmail.com>
2020-07-24 22:59:50 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="MCMC algorithms to identify linguistic contact zones",
     author="Peter Ranacher",
     author_email="peter.ranacher@geo.uzh.ch",
-    long_description=open('README.md').read(),
+    #long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     keywords='data linguistics',
     license='???',
