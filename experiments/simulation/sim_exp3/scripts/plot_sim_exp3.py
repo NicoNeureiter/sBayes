dcb1a850749024f791835c7ce7e810e88a5b8ca0
derpetermann <peter.ranacher@gmail.com>
2020-09-26 17:10:22 +0200

    scheissiregdnwas

--- experiments/simulation/sim_exp3/scripts/plot_sim_exp3.py
+++ experiments/simulation/sim_exp3/scripts/plot_sim_exp3.py
@@ -21,19 +21,19 @@ if __name__ == '__main__':
         plt.read_results(model=m)
         results_per_model[m] = plt.results
 
-        # # Plot Maps
-        # try:
-        #     plt.posterior_map(
-        #         post_freq_legend=[1, 0.75, 0.5],
-        #         post_freq=0.9,
-        #         burn_in=0.7,
-        #         plot_families=False,
-        #         plot_area_stats=False,
-        #         add_overview=False,
-        #         label_languages=False,
-        #         fname='/posterior_map_' + m)
-        # except ValueError:
-        #     pass
+        # Plot Maps
+        try:
+            plt.posterior_map(
+                post_freq_legend=[1, 0.75, 0.5],
+                post_freq=0.9,
+                burn_in=0.7,
+                plot_families=False,
+                plot_area_stats=False,
+                add_overview=False,
+                label_languages=False,
+                fname='/posterior_map_' + m)
+        except ValueError:
+            pass
 
     models.plot_dic(results_per_model, burn_in=0.5, true_model='n4')
     models.plot_recall_precision_over_several_models(results_per_model)
