06153d2407bda67f8be5f769ab0887df3c5e3bdc
derpetermann <peter.ranacher@gmail.com>
2020-10-02 09:29:06 +0200

    reworking the plotting configs

--- experiments/south_america/scripts/plot_sa_exp.py
+++ experiments/south_america/scripts/plot_sa_exp.py
@@ -25,15 +25,7 @@ if __name__ == '__main__':
 
         # Plot Maps
         try:
-            map.posterior_map(
-                post_freq_legend=[0.8, 0.6, 0.4],
-                post_freq=0.7,
-                burn_in=0.2,
-                plot_families=True,
-                plot_area_stats=True,
-                add_overview=True,
-                label_languages=True,
-                fname='/posterior_map_' + m)
+            map.posterior_map(file_name='/posterior_map_' + m)
         except ValueError:
             pass
 
@@ -44,10 +36,11 @@ if __name__ == '__main__':
 
         labels = ['U', 'C', 'I']
 
+
         plot_general = GeneralPlot()
         plot_general.load_config(config_file='../config_plot.json')
         # In this case, we don't need to use load_results
-        plot_general.results = map.results
+        plot_general.results = plot_map.results
         plot_general.plot_probability_grid(burn_in=0.5, fname='/prob_grid_' + m)
         plot_general.plot_weights_grid(labels=labels, burn_in=0.5, fname='/weights_grid_' + m)
 
