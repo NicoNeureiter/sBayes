2ef161a1fc9637798ec9a61ebe2da824fa86125d
derpetermann <peter.ranacher@gmail.com>
2020-11-26 23:32:35 +0100

    sim_exp1 plotting functions

--- experiments/simulation/sim_exp1/scripts/plot_sim_exp1.py
+++ experiments/simulation/sim_exp1/scripts/plot_sim_exp1.py
@@ -1,41 +1,35 @@
 import warnings
-from sbayes.plotting.map import Map
-from sbayes.plotting.general_plot import GeneralPlot
+from sbayes.plot import Plot
 
 warnings.simplefilter(action='ignore', category=UserWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
+
 
 if __name__ == '__main__':
-    results_per_model = {}
-    models = GeneralPlot()
-    models.load_config(config_file='../results/results_server/config_plot.json')
+    plot = Plot(simulated_data=True)
+    plot.load_config(config_file='../results/results_server/config_plot.json')
+    plot.read_data()
 
     # Get model names
-    names = models.get_model_names()
-    map = None
+    names = plot.get_model_names()
+
     for m in names:
 
-        map = Map(simulated_data=True)
-        map.load_config(config_file='../results/results_server/config_plot.json')
-        # Read sites, sites_names, network
-        map.read_data()
         # Read results for each model
-        map.read_results(model=m)
+        plot.read_results(model=m)
+
+        # Plot map
         try:
-            map.posterior_map(file_name='posterior_map_' + m, return_correspondence=True)
+            plot.posterior_map(file_name='posterior_map_' + m, return_correspondence=True)
         except ValueError:
             pass
-        results_per_model[m] = map.results
 
-        plt = GeneralPlot()
-        plt.load_config(config_file='../results/results_server/config_plot.json')
+        plot.config['plot_trace']['parameter'] = 'recall_and_precision'
+        plot.config['plot_trace']['ground_truth']['add'] = False
+        plot.plot_trace(file_name="trace_recall_precision_" + m)
 
-        # # In this case, we don't need to use load_results
-        plt.results = map.results
-        plt.config['plot_trace']['parameter'] = 'recall_and_precision'
-        plt.config['plot_trace']['ground_truth']['add'] = False
-        plt.plot_trace(file_name="trace_recall_precision_" + m)
-
-        plt.config['plot_trace']['parameter'] = 'likelihood'
-        plt.config['plot_trace']['ground_truth']['add'] = True
-        plt.plot_trace(file_name="trace_likelihood_" + m)
+        plot.config['plot_trace']['parameter'] = 'likelihood'
+        plot.config['plot_trace']['ground_truth']['add'] = True
+        plot.plot_trace(file_name="trace_likelihood_" + m)
 
