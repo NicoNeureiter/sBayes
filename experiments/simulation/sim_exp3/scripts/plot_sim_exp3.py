901d165df79bee55eb001c58aa7a08014c9ac6c2
derpetermann <peter.ranacher@gmail.com>
2020-11-26 23:59:19 +0100

    sim_exp3 plotting functions

--- experiments/simulation/sim_exp3/scripts/plot_sim_exp3.py
+++ experiments/simulation/sim_exp3/scripts/plot_sim_exp3.py
@@ -1,35 +1,31 @@
-from sbayes.plotting.map import Map
-from sbayes.plotting.general_plot import GeneralPlot
-
-
 import warnings
-from sbayes.plotting.map import Map
-from sbayes.plotting.general_plot import GeneralPlot
+from sbayes.plot import Plot
 
 warnings.simplefilter(action='ignore', category=UserWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
+
 
 if __name__ == '__main__':
-    results_per_model = {}
-    models = GeneralPlot()
-    models.load_config(config_file='../results/results_server/config_plot.json')
+    plot = Plot(simulated_data=True)
+    plot.load_config(config_file='../results/results_server/config_plot.json')
+    plot.read_data()
 
     # Get model names
-    names = models.get_model_names()
-    map = None
+    names = plot.get_model_names()
+    results_per_model = {}
+
     for m in names:
 
-        map = Map(simulated_data=True)
-        map.load_config(config_file='../results/results_server/config_plot.json')
-        # Read sites, sites_names, network
-        map.read_data()
         # Read results for each model
-        map.read_results(model=m)
+        plot.read_results(model=m)
+        
+        # Plot map
         try:
-            map.posterior_map(file_name='posterior_map_' + m, return_correspondence=False)
+            plot.posterior_map(file_name='posterior_map_' + m, return_correspondence=False)
         except ValueError:
             pass
-        results_per_model[m] = map.results
+        results_per_model[m] = plot.results
 
-    models.plot_dic(results_per_model, file_name='dic')
-    models.plot_recall_precision_over_all_models(results_per_model, file_name='recall_precision_over_all_models')
-
+    plot.plot_dic(results_per_model, file_name='dic')
+    plot.plot_recall_precision_over_all_models(results_per_model, file_name='recall_precision_over_all_models')
