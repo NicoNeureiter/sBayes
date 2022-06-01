cd9394e3e3eeedcc0b4563a8615ceaf6293c4e88
NicoNeureiter <nico.neureiter@gmail.com>
2022-06-01 15:13:23 +0200

    enable --threads option in CLI

--- sbayes/cli.py
+++ sbayes/cli.py
@@ -13,67 +13,79 @@ from sbayes.mcmc_setup import MCMCSetup
 
 
 def run_experiment(
-        experiment: Experiment,
-        data: Data,
-        i_run: int,
-        custom_settings: dict = None,
+    config: PathLike,
+    experiment_name: str,
+    custom_settings: dict = None,
+    i_run: int = 0,
 ):
-    if custom_settings is not None:
-        experiment.config.update(custom_settings)
+    # Initialize the experiment
+    experiment = Experiment(
+        experiment_name=experiment_name,
+        config_file=config,
+        custom_settings=custom_settings,
+        log=True,
+    )
 
+    # Experiment based on a specified (in config) data-set
+    data = Data.from_experiment(experiment)
+
+    # Set up MCMC
     mcmc = MCMCSetup(data=data, experiment=experiment)
     mcmc.log_setup()
 
-    # Warm-up
+    # Warm-up and run MCMC sampling
     mcmc.warm_up()
-
-    # Sample from posterior
     mcmc.sample(run=i_run)
 
     # Use the last sample as the new initial sample
     return mcmc.samples.last_sample
 
 
+def runner(args):
+    """A wrapper for `run_experiment` to make it callable using the pool.map interface."""
+    i_run, n_clusters, config, experiment_name = args
+    # run_experiment(config, f"{experiment_name}/K{n_clusters}_{i_run}",
+    run_experiment(
+        config=config,
+        experiment_name=experiment_name,
+        custom_settings={"model": {"clusters": n_clusters}, "mcmc": {"runs": 1}},
+        i_run=i_run,
+    )
+
+
 def main(
-        config: PathLike,
-        experiment_name: str = None,
-        custom_settings: dict = None,
-        processes: int = 1,
+    config: PathLike,
+    experiment_name: str = None,
+    custom_settings: dict = None,
+    processes: int = 1,
 ):
     # Initialize the experiment
-    experiment = Experiment(experiment_name=experiment_name,
-                            config_file=config,
-                            custom_settings=custom_settings,
-                            log=True)
+    experiment = Experiment(
+        experiment_name=experiment_name,
+        config_file=config,
+        custom_settings=custom_settings,
+        log=False,
+    )
 
-    # Experiment based on a specified (in config) data-set
-    data = Data.from_experiment(experiment)
-    data.load_geo_cost_matrix()
-
-    # Rerun experiment to check for consistency
-    i_run_range = list(range(experiment.config['mcmc']['runs']))
-    n_clusters_range = experiment.config['model']['clusters']
+    # Extract the range of run repetitions and number of clusters
+    i_run_range = list(range(experiment.config.mcmc.runs))
+    n_clusters_range = experiment.config.model.clusters
     if type(n_clusters_range) not in [tuple, list, set]:
         assert isinstance(n_clusters_range, int)
         n_clusters_range = [n_clusters_range]
 
-    def runner(i_run, n_clusters):
-        run_experiment(
-            experiment=experiment,
-            data=data,
-            i_run=i_run,
-            custom_settings={'model': {'clusters': n_clusters}}
-        )
+    # Define configurations for each distinct sBayes run that needs to be executed
+    run_configurations = product(
+        i_run_range, n_clusters_range, [config], [experiment.experiment_name]
+    )
 
-    for i in i_run_range:
-        for k in n_clusters_range:
-            runner(i, k)
-
-    # if processes <= 1:
-    #     map(runner, product(i_run_range, n_clusters_range))
-    # else:
-    #     pool = multiprocessing.Pool(processes=processes)
-    #     pool.map(runner, product(i_run_range, n_clusters_range))
+    # Run all configurations sequentially or in parallel
+    if processes <= 1:
+        for cfg in run_configurations:
+            runner(cfg)
+    else:
+        pool = multiprocessing.Pool(processes=processes)
+        pool.map(runner, run_configurations)
 
 
 def iterate_over_parameter(values, config_setter, function, print_message=None):
@@ -98,33 +110,37 @@ def iterate_or_run(x, config_setter, function, print_message=None):
 def cli():
     """Command line interface."""
     parser = argparse.ArgumentParser(
-        description='An MCMC algorithm to identify contact zones')
-    parser.add_argument('config', type=Path,
-                        help='The JSON configuration file')
-    parser.add_argument('name', nargs='?', type=str,
-                        help='The experiment name used for logging and as the name of the results directory.')
-    parser.add_argument('threads', nargs='?', type=int, default=1,
-                        help='The number of parallel processes.')
+        description="An MCMC algorithm to identify contact zones"
+    )
+    parser.add_argument("config", type=Path, help="The JSON configuration file")
+    parser.add_argument(
+        "--name",
+        nargs="?",
+        type=str,
+        help="The experiment name used for logging and as the name of the results directory.",
+    )
+    parser.add_argument(
+        "--threads",
+        nargs="?",
+        type=int,
+        default=1,
+        help="The number of parallel processes.",
+    )
 
     args = parser.parse_args()
-
     config = args.config
 
     # Ask for config file via files-dialog, if not provided as argument.
     if config is None:
         tk.Tk().withdraw()
         config = filedialog.askopenfilename(
-            title='Select a config file in JSON format.',
-            initialdir='..',
-            filetypes=(('json files', '*.json'), ('all files', '*.*'))
+            title="Select a config file in JSON format.",
+            initialdir="..",
+            filetypes=(("json files", "*.json"), ("all files", "*.*")),
         )
 
-    main(
-        config=config,
-        experiment_name=args.name,
-        processes=args.threads
-    )
+    main(config=config, experiment_name=args.name, processes=args.threads)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
