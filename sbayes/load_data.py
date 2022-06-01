879d9d0e3b31241ba873cf7b483961442fa66dcc
NicoNeureiter <nico.neureiter@gmail.com>
2022-06-01 15:39:31 +0200

    improved interface of Data initialization

--- sbayes/load_data.py
+++ sbayes/load_data.py
@@ -6,7 +6,8 @@ import pandas as pd
 import pyproj
 from dataclasses import dataclass
 from logging import Logger
-from typing import List, Tuple, Dict, Optional
+from pathlib import Path
+from typing import List, Tuple, Dict, Optional, Union, Sequence
 
 
 try:
@@ -27,9 +28,9 @@ class Objects:
     """Container class for a set of objects. Each object describes one sample (a language,
     person, state,...) which has an ID, name and location."""
 
-    id: List[int]
-    locations: Tuple[float, float]
-    names: List[str]
+    id: Sequence[int]
+    locations: NDArray[float]  # shape: (n_sites, 2)
+    names: Sequence[str]
 
     def __getitem__(self, key):
         return getattr(self, key)
@@ -43,12 +44,12 @@ class Objects:
         return self._indices
 
     @classmethod
-    def from_dataframe(cls, data: pd.DataFrame) -> 'Objects':
+    def from_dataframe(cls, data: pd.DataFrame) -> "Objects":
         n_sites = data.shape[0]
         try:
-            x = data['x']
-            y = data['y']
-            id_ext = data['id'].tolist()
+            x = data["x"]
+            y = data["y"]
+            id_ext = data["id"].tolist()
             # id_ext = np.arange(n_sites)
         except KeyError:
             raise KeyError("The csv must contain columns `x`, `y` and `id`")
@@ -60,60 +61,59 @@ class Objects:
             locations[i, 1] = float(y[i])
 
         objects_dict = {
-            'locations': locations,
-            'id': id_ext,
-            'names': list(data.get('name', id_ext))
+            "locations": locations,
+            "id": id_ext,
+            "names": list(data.get("name", id_ext)),
         }
         return cls(**objects_dict)
 
 
 @dataclass
 class Features:
-    values: NDArray[bool]
-    names: NDArray[str]
-    states: NDArray[bool]
-    state_names: List[List[str]]
+
+    values: NDArray[bool]  # shape: (n_sites, n_features, n_states)
+    names: NDArray[str]  # shape: (n_features,)
+    states: NDArray[bool]  # shape: (n_features, n_states)
+    state_names: List[List[str]]  # shape for each feature f: (n_states[f],)
     na_number: int
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> Union[NDArray, List, int]:
         return getattr(self, key)
 
     @property
-    def n_sites(self):
+    def n_sites(self) -> int:
         return self.values.shape[0]
 
     @property
-    def n_features(self):
+    def n_features(self) -> int:
         return self.values.shape[1]
 
     @property
-    def n_states_per_feature(self):
+    def n_states_per_feature(self) -> List[int]:
         return [sum(applicable) for applicable in self.states]
 
     @classmethod
     def from_dataframes(
-            cls: type,
-            data: pd.DataFrame,
-            feature_states: pd.DataFrame,
+        cls: type,
+        data: pd.DataFrame,
+        feature_states: pd.DataFrame,
     ):
         feature_data = data.loc[:, feature_states.columns]
         features_dict, na_number = encode_states(feature_data, feature_states)
-        features_dict['names'] = feature_states.columns.to_numpy()
+        features_dict["names"] = feature_states.columns.to_numpy()
         return cls(**features_dict, na_number=na_number)
 
 
 @dataclass
 class Confounder:
     name: str
-    group_assignment: NDArray[bool]
-    # shape: (n_groups, n_sites)
-    group_names: NDArray[str]
-    # shape: (n_groups,)
+    group_assignment: NDArray[bool]  # shape: (n_groups, n_sites)
+    group_names: NDArray[str]  # shape: (n_groups,)
 
     def __getitem__(self, key):
-        if key == 'names':
+        if key == "names":
             return self.group_names
-        elif key == 'values':
+        elif key == "values":
             return self.group_assignment
         return getattr(self, key)
 
@@ -123,10 +123,10 @@ class Confounder:
 
     @classmethod
     def from_dataframe(
-            cls: type,
-            confounder_name: str,
-            group_names: List[str],
-            data: pd.DataFrame,
+        cls: type,
+        confounder_name: str,
+        group_names: List[str],
+        data: pd.DataFrame,
     ):
         n_sites = data.shape[0]
 
@@ -135,13 +135,17 @@ class Confounder:
                 # Special case: this effect applies to all objects in the same way and
                 # does not require a separate column in the data file.
                 group_assignment = np.ones((1, n_sites), dtype=bool)
-                return cls(name=confounder_name,
-                           group_assignment=group_assignment,
-                           group_names=group_names)
+                return cls(
+                    name=confounder_name,
+                    group_assignment=group_assignment,
+                    group_names=group_names,
+                )
 
             else:
-                raise KeyError(f"The config file lists \'{confounder_name}\' as a confounder. Remove "
-                               f"confounder or include \'{confounder_name}\' in the features.csv file.")
+                raise KeyError(
+                    f"The config file lists '{confounder_name}' as a confounder. Remove "
+                    f"confounder or include '{confounder_name}' in the features.csv file."
+                )
 
         group_names_by_site = data[confounder_name]
         group_names = list(np.unique(group_names_by_site.dropna()))
@@ -149,9 +153,11 @@ class Confounder:
         for g, g_name in enumerate(group_names):
             group_assignment[g, np.where(group_names_by_site == g_name)] = True
 
-        return cls(name=confounder_name,
-                   group_assignment=group_assignment,
-                   group_names=group_names)
+        return cls(
+            name=confounder_name,
+            group_assignment=group_assignment,
+            group_names=group_names,
+        )
 
 
 class Data:
@@ -160,86 +166,74 @@ class Data:
     sBayes analysis.
     """
 
-    path_results: PathLike
-    experiment_name: str
-    config: 'SBayesConfig'
-    crs: Optional[pyproj.CRS]
     objects: Objects
     features: Features
     confounders: Dict[str, Confounder]
-    network: ComputeNetwork
     prior_confounders: ...
+    crs: Optional[pyproj.CRS]
     geo_cost_matrix: Optional[NDArray[float]]
+    network: ComputeNetwork
     logger: Logger
 
     def __init__(
-            self,
-            path_results: PathLike,
-            experiment_name: str,
-            config: 'SBayesConfig',
-            crs: Optional[pyproj.CRS],
-            geo_cost_matrix: Optional[NDArray[float]] = None,
-            logger: Logger = None,
+        self,
+        objects: Objects,
+        features: Features,
+        confounders: Dict[str, Confounder],
+        projection: Optional[str] = "epsg:4326",
+        geo_costs: Union[Literal["from_data"], PathLike] = "from_data",
+        logger: Logger = None,
     ):
-        self.path_results = path_results
-        self.experiment_name = experiment_name
-        self.config = config
-        self.crs = crs
-
-        # Optional attributes
-        self.geo_cost_matrix = geo_cost_matrix
+        self.objects = objects
+        self.features = features
+        self.confounders = confounders
         self.logger = logger
 
-        # Attributes that are loaded based on config
-        self.objects, self.features, self.confounders = read_features_from_csv(
-            data_path=self.config.data.features,
-            feature_states_path=self.config.data.feature_states,
-            groups_by_confounder=self.config.model.confounders,
-            logger=self.logger)
-
+        self.crs = pyproj.CRS(projection)
         self.network = ComputeNetwork(self.objects, crs=self.crs)
+
+        if geo_costs == "from_data":
+            self.geo_cost_matrix = self.network.dist_mat
+        else:
+            self.geo_cost_matrix = read_geo_cost_matrix(
+                object_names=self.objects.names, file=geo_costs, logger=self.logger
+            )
 
         self.prior_confounders = None  # TODO Load the confounder priors
 
     @classmethod
-    def from_experiment(cls, experiment: 'Experiment') -> 'Data':
-        proj4_string = experiment.config.data.projection
-        if proj4_string is None:
-            crs = None
-        else:
-            crs = pyproj.CRS(proj4_string)
-
-        return cls(
-            path_results=experiment.path_results,
-            experiment_name=experiment.experiment_name,
-            config=experiment.config,
-            crs=crs,
-            logger=experiment.logger,
+    def from_config(cls, config: "SBayesConfig", logger=None) -> "Data":
+        # Load objects, features, confounders
+        objects, features, confounders = read_features_from_csv(
+            data_path=config.data.features,
+            feature_states_path=config.data.feature_states,
+            groups_by_confounder=config.model.confounders,
+            logger=logger,
         )
+
+        # Create a Data object using __init__
+        return cls(
+            objects=objects,
+            features=features,
+            confounders=confounders,
+            projection=config.data.projection,
+            geo_costs=config.model.prior.geo.costs,
+            logger=logger,
+        )
+
+    @classmethod
+    def from_experiment(cls, experiment: "Experiment") -> "Data":
+        return cls.from_config(experiment.config, logger=experiment.logger)
 
     def load_features(self):
         # Empty function for backwards compatibility. Loading now happens in initialization
-        # TODO
+        # TODO remove
         pass
 
     def load_geo_cost_matrix(self):
-        geo_prior_cfg = self.config.model.prior.geo
-        if geo_prior_cfg.type != 'cost_based':
-            # Geo prior is not cost-based -> nothing to do
-            return
-
-        if 'costs' not in geo_prior_cfg:
-            geo_prior_cfg.costs = 'from_data'
-
-        if geo_prior_cfg.costs == 'from_data':
-            # No cost-matrix given. Use distance matrix as costs
-            self.geo_cost_matrix = self.network.dist_mat
-
-        else:
-            # Read cost matrix from data
-            self.geo_cost_matrix = read_geo_cost_matrix(object_names=self.objects.names,
-                                                        file=geo_prior_cfg.costs,
-                                                        logger=self.logger)
+        # Empty function for backwards compatibility. Loading now happens in initialization
+        # TODO remove
+        pass
 
 
 @dataclass
@@ -252,10 +246,10 @@ class Prior:
 
 
 def read_features_from_csv(
-        data_path: PathLike,
-        feature_states_path: PathLike,
-        groups_by_confounder: Dict[str, list],
-        logger: Optional[Logger] = None,
+    data_path: PathLike,
+    feature_states_path: PathLike,
+    groups_by_confounder: Dict[str, list],
+    logger: Optional[Logger] = None,
 ) -> (Objects, Features, Dict[str, Confounder]):
     """This is a helper function to import data (objects, features, confounders) from a csv file
     Args:
@@ -279,8 +273,12 @@ def read_features_from_csv(
     }
 
     if logger:
-        logger.info(f"{features.n_sites} sites with {features.n_features} features read from {data_path}.")
+        logger.info(
+            f"{features.n_sites} sites with {features.n_features} features read from {data_path}."
+        )
         logger.info(f"{features.na_number} NA value(s) found.")
-        logger.info(f"The maximum number of states in a single feature was {feature_states.shape[0]}.")
+        logger.info(
+            f"The maximum number of states in a single feature was {feature_states.shape[0]}."
+        )
 
     return objects, features, confounders
